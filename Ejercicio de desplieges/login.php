7c1c029438b98b49baa77e8ca4f712b6596cd940
RafaelSilva27 <radasilva789@gmail.com>
2023-11-14 16:09:29 +0100

    commit

--- Ejercicio de desplieges/login.php
+++ Ejercicio de desplieges/login.php
@@ -1,20 +1,22 @@
-<?
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST["usuario"];
     $password = $_POST["password"];
 
-    $usuarios = array("usuario1", "usuario2", "usuario3");
-    $passwords = array("pass1", "pass2", "pass3");
+    $usuarios = array("usuario1", "usuario2", "usuario3", "paco");
+    $passwords = array("pass1", "pass2", "pass3", "pass4");
 
     $login = array_search($usuario, $usuarios);
 
     if ($login != false && $password == $passwords[$login]) {
-      header("Location: logueado.html");
+        $_SESSION['usuario'] = $usuario;
+        header("Location: logueado.html");
+        exit();
     } else {
-      echo "Usuario o contraseña incorrectos";
-      //header("Location: login.html");
+        echo "Usuario o contraseña incorrectos" . "<br>";
+        echo "<a href=\"login.html\">Iniciar sesión</a>";
     }
-  }
+}
 ?>
-
